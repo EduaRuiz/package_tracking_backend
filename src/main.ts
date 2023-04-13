cc65b5faea36110c716cff57e919471387214bbf
ERuiz <edwar_775@hotmail.com>
2023-04-13 15:37:43 -0500

    app.enableCors(), se acomoda el cors generico

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: [
-      'https://package-tracking-d5e6a.web.app',
-      'https://www.google.com',
-      'http://localhost:4200',
-    ],
-    methods: ['POST', 'PUT', 'DELETE', 'GET', 'PATCH'],
-  });
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
